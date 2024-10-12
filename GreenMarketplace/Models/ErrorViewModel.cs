ef8613f38de6373bd3b25056c77e88dba43c8027
1997Pratham <Vipin1591997@gmail.com>
2024-10-12 14:16:17 +0530

    Add project files.

---
+++ GreenMarketplace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GreenMarketplace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
